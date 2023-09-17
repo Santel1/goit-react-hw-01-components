7388b43b5c920abfd1f650b40d3fc047db454a21
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-09-17 13:09:07 +0200

    hw-1-react

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,36 +1,8 @@
 import css from './Statistics.module.css';
-// export const Statistic = props => {
-//   return (
-//     <section className={css.statistics}>
-//       <h2 className={css.title}>{props.title}</h2>
-//       <ul className={css.statList}>{props.children}</ul>
-//     </section>
-//   );
-// };
-
-// export const StatisticItem = props => {
-//   return (
-//     <li
-//       className={css.item}
-//       style={{
-//         backgroundColor: getRandomHexColor(),
-//       }}
-//     >
-//       <span className={css.label}>{props.label}</span>
-//       <span className={css.percentage}>{props.percentage}%</span>
-//     </li>
-//   );
-// };
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
 export const Statistic = ({ title, data }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         {data.map(({ id, label, percentage }) => {
           return (
